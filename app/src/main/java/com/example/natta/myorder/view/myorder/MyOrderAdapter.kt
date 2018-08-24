592fcf17d2da70f4d13ba048abf282f8fd790eff
NattapongPallawal <nattapong_pallawal@hotmail.com>
2018-08-25 01:48:10 +0700

    add payment

--- app/src/main/java/com/example/natta/myorder/view/myorder/MyOrderAdapter.kt
+++ app/src/main/java/com/example/natta/myorder/view/myorder/MyOrderAdapter.kt
@@ -7,21 +7,21 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.natta.myorder.R
 
-class MyOrderAdapter(var context:Context) : RecyclerView.Adapter<MyOrderAdapter.MyorderViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyorderViewHolder {
+class MyOrderAdapter(var context:Context) : RecyclerView.Adapter<MyOrderAdapter.MyOrderViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyOrderViewHolder {
         val view = LayoutInflater.from(context).inflate(R.layout.list_my_order,parent,false)
-        return MyorderViewHolder(view)
+        return MyOrderViewHolder(view)
     }
 
     override fun getItemCount(): Int {
         return 100
     }
 
-    override fun onBindViewHolder(holder: MyorderViewHolder, position: Int) {
+    override fun onBindViewHolder(holder: MyOrderViewHolder, position: Int) {
     }
 
 
-    inner class MyorderViewHolder(view : View) : RecyclerView.ViewHolder(view) {
+    inner class MyOrderViewHolder(view : View) : RecyclerView.ViewHolder(view) {
 
     }
 }
