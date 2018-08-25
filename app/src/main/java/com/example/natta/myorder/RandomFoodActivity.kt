b44d54fdd94dedb34be92ed4b57733d307d123ea
NattapongPallawal <nattapong_pallawal@hotmail.com>
2018-08-25 17:29:46 +0700

    add random food

---
+++ app/src/main/java/com/example/natta/myorder/RandomFoodActivity.kt
@@ -0,0 +1,12 @@
+package com.example.natta.myorder
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class RandomFoodActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_random_food)
+    }
+}
