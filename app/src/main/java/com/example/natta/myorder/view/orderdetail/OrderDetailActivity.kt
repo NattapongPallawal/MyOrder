b9d04a2b3202b46d0f339e5dd3db0f32f9974f07
NattapongPallawal <nattapong_pallawal@hotmail.com>
2018-08-26 02:06:52 +0700

    add back navigation in Order Detail

--- app/src/main/java/com/example/natta/myorder/view/orderdetail/OrderDetailActivity.kt
+++ app/src/main/java/com/example/natta/myorder/view/orderdetail/OrderDetailActivity.kt
@@ -1,7 +1,7 @@
 package com.example.natta.myorder.view.orderdetail
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
 import com.example.natta.myorder.R
 
 class OrderDetailActivity : AppCompatActivity() {
@@ -9,5 +9,12 @@ class OrderDetailActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_order_detail)
+        val actionBar = supportActionBar
+        actionBar!!.setDisplayHomeAsUpEnabled(true)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        onBackPressed()
+        return super.onSupportNavigateUp()
     }
 }
